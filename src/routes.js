2cb0b2cd8163a15b07fee3b04d93cbbcf2347174
Rafael Ramires <moreira.ramires@gmail.com>
2021-02-08 09:59:33 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const express= require('express')
+const routes = express.Router()
+const instructors = require('./app/controllers/instructors.routes')
+const members = require('./app/controllers/members.routes')
+
+
+routes.get('/',function(req,res){
+  return res.redirect('/instructors')
+  
+})
+
+routes.get('/instructors',instructors.index)
+routes.put('/instructors/', instructors.put)
+routes.post('/instructors',instructors.post)
+routes.delete('/instructors',instructors.delete)
+routes.get('/instructors/create',instructors.create)
+routes.get('/instructors/:id', instructors.show)
+routes.get('/instructors/:id/edit', instructors.edit)
+
+
+routes.get('/members',members.index)
+routes.put('/members/', members.put)
+routes.post('/members',members.post)
+routes.delete('/members',members.delete)
+routes.get('/members/create',members.create)
+routes.get('/members/:id', members.show)
+routes.get('/members/:id/edit', members.edit)
+
+
+
+
+module.exports = routes
