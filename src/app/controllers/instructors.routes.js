98f522482fa87ae0650cbbf14ca9f3f2c7289530
Rafael Ramires <moreira.ramires@gmail.com>
2021-02-15 10:55:49 -0300

    Criando models e aprofundando em callback function

--- src/app/controllers/instructors.routes.js
+++ src/app/controllers/instructors.routes.js
@@ -1,18 +1,12 @@
-const {age, date} = require("../../lib/utils")
-const db = require('../../config/db')
+const Instructor = require('../models/Instructor')
 
 module.exports ={
   index(req,res){ 
-
-    db.query(`SELECT * FROM instructors`, function(err,results){
-
-       if(err) return res.send(`Database Error!`)
-
+      Instructor.all(function(instructors){
+        return res.render('instructors/index', {instructors})
       
-      return res.render('instructors/index', {instructors:results.rows})
-    })
-    
-  
+      })
+   
   },
   create(req,res){
     return res.render('instructors/create')
@@ -24,36 +18,12 @@ module.exports ={
         return res.send('Pls, fill all fields')
       }
     }
-    
-    const query = ` 
-      INSERT INTO instructors ( 
-        name,
-        avatar_url,
-        gender,
-        services,
-        birth,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6 )
-      RETURNING id
-    `
-        const values =[ 
-          req.body.name,
-          req.body.avatar_url,
-          req.body.gender,
-          req.body.services,
-          date(req.body.birth).iso,
-          date(Date.now()).iso
-        ]
 
-    db.query(query,values,function(err,results){
-      if(err) return res.send(`Database Error!`)
-        return  res.redirect(`/instructors/${results.row[0].id}`)
+     Instructor.create(req.body, function(Instructor){
+       return  res.redirect(`/instructors/${instructor.id}`)
+     })
 
-      
-      return
-      })
-      
-     
+
   },
   show(req,res){ 
     return
